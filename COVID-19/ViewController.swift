aea92121d42f60c3cc24fb79b50c6d6ceea64100
jsa0224 <puccav@naver.com>
2022-03-04 12:29:18 +0900

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by 정선아 on 2022/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
